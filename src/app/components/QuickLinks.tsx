9b3826cfe37bb857d91d76df9a901d59c330abef
Ansh Raj <146376691+AnshRaj112@users.noreply.github.com>
2025-04-28 00:57:04 +0530

    Fixing stuff

--- src/app/components/QuickLinks.tsx
+++ src/app/components/QuickLinks.tsx
@@ -107,7 +107,7 @@ const QuickLinks: React.FC = () => {
   ];
 
   return (
-    <div className="max-w-5xl mx-auto px-4 py-12">
+    <div className="max-w-5xl mx-auto px-1 py-2">
       {/* Divider Around Toggle Button */}
       <div className="flex justify-center items-center mb-3">
         <div className="flex-grow h-0.5 bg-gradient-to-l from-[#0097b2] to-transparent"></div>
